65b647efe5dda4d127eda95c2e4174b4d6f6ddef
zhengze <898601566@qq.com>
2021-08-03 18:05:42 +0800

    限流

--- app/exception/SystemException.php
+++ app/exception/SystemException.php
@@ -17,6 +17,7 @@ use Fastswoole\core\BaseException;
  */
 class SystemException extends BaseException
 {
+
     const SYSTEM_ERROR = ['msg' => '系统错误', 'code' => 10000,];
     const MODULE_DOES_NOT_EXIST = ['msg' => '模块不存在', 'code' => 10001,];
     const CONTROLLER_DOES_NOT_EXIST = ['msg' => '控制器不存在', 'code' => 10002,];
@@ -25,4 +26,5 @@ class SystemException extends BaseException
     const TIMESTAMP_OUT = ['msg' => '时间戳超时', 'code' => 10005,];
     const SIGNATURE_ERROR = ['msg' => '签名错误', 'code' => 10006,];
     const SERVER_TIMEOUT = ['msg' => '服务器超时,请稍后再试', 'code' => 10007,];
+
 }
