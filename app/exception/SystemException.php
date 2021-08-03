8dec5e16db675228c9c9476f9d6ffe64ea4711bf
zhengze <898601566@qq.com>
2021-08-03 18:00:45 +0800

    停止维护

--- app/exception/SystemException.php
+++ app/exception/SystemException.php
@@ -24,4 +24,5 @@ class SystemException extends BaseException
     const TIMESTAMP_CANNOT_BE_EMPTY = ['msg' => '时间戳不能为空', 'code' => 10004,];
     const TIMESTAMP_OUT = ['msg' => '时间戳超时', 'code' => 10005,];
     const SIGNATURE_ERROR = ['msg' => '签名错误', 'code' => 10006,];
+    const SERVER_TIMEOUT = ['msg' => '服务器超时,请稍后再试', 'code' => 10007,];
 }
