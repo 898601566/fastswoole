89de474afaa37fda4e82ba27e5f1e2e5d3ae9e5f
zhengze <898601566@qq.com>
2021-08-03 14:50:31 +0800

    加签

--- app/exception/SystemException.php
+++ app/exception/SystemException.php
@@ -21,4 +21,7 @@ class SystemException extends BaseException
     const MODULE_DOES_NOT_EXIST = ['msg' => '模块不存在', 'code' => 10001,];
     const CONTROLLER_DOES_NOT_EXIST = ['msg' => '控制器不存在', 'code' => 10002,];
     const METHOD_DOES_NOT_EXIST = ['msg' => '方法不存在', 'code' => 10003,];
+    const TIMESTAMP_CANNOT_BE_EMPTY = ['msg' => '时间戳不能为空', 'code' => 10004,];
+    const TIMESTAMP_OUT = ['msg' => '时间戳超时', 'code' => 10005,];
+    const SIGNATURE_ERROR = ['msg' => '签名错误', 'code' => 10006,];
 }
